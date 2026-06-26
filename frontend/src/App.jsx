7a64b815892acdf5996acd492a701522757f0133
DavidC2205 <davidcastillo2205@gmail.com>
2026-06-25 22:00:58 -0500

    feat(FS0005-13): implement base layout components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Layout from "./components/layout/Layout";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </BrowserRouter>
-    );
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
 }
-
-export default App;
---
+++ frontend/src/components/layout/Layout.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+
+      <main className="flex-1 pt-20">
+        {children}
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
